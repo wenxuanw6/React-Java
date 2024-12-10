4261e3fa38462040e01c97c089c90c9388f3846d
Leo Zhang <lzhang@atpco.net>
2024-12-10 17:22:32 +0800

    created project

---
+++ app/Page1.tsx
@@ -0,0 +1,11 @@
+const Page1 = () => {
+    return (
+      <div>
+        <h2>Page 1</h2>
+        <p>This is the content for Page 1.</p>
+      </div>
+    );
+  };
+  
+  export default Page1;
+  
---
+++ app/Page2.tsx
@@ -0,0 +1,10 @@
+const Page2 = () => {
+    return (
+      <div>
+        <h2>Page 2</h2>
+        <p>This is the content for Page 2.</p>
+      </div>
+    );
+  };
+  
+  export default Page2;
